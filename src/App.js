b7c6cbcdf83524749bc3d8084ff7d3f1abe1e48f
SureshSubedii <sureshsubedi485@gmail.com>
2023-03-06 11:50:06 +0545

    Redux

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import Navbar from './Navbar';
+import {increment,decrement} from './features/counterSlice'
 
 function App() {
+  const dispatch=useDispatch();
+  const handleclick1=()=>{
+    dispatch( decrement()
+)
+
+
+  }
+  const handleclick2=()=>{
+    dispatch(increment())
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Navbar/>
+      <h1>Learnig Redux</h1>
+      <div onClick={handleclick1} className="btn btn-primary mx-3"> -</div>
+      <b>Add/Sub</b>
+
+      <div onClick={handleclick2} className="btn btn-primary mx-3"> +</div>
+
+
     </div>
   );
 }
